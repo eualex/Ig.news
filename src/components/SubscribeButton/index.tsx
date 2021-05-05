0182d0c6d2a0a7ef08ad82749ddea9fea4c7a574
alex.matos <alex.matos@ceuma.br>
2021-05-05 18:57:01 -0300

    feat: habilitando inscrição pelo front da aplicação

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,7 @@
+import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJs } from '../../services/stripe-js'
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -7,8 +11,32 @@ interface SubscribeButtonProps {
 export function SubscribeButton({
   priceId
 }: SubscribeButtonProps): JSX.Element {
+  const [session] = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId })
+    } catch (err) {
+      alert(err.message)
+    }
+  }
+
   return (
-    <button className={styles.subscribeButton} type="button">
+    <button
+      className={styles.subscribeButton}
+      type="button"
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   )
