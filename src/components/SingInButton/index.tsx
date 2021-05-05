3992be2f5c7b376bbed140904090608476695bfd
alex.matos <alex.matos@ceuma.br>
2021-05-05 11:18:24 -0300

    feat: criando autenticação da aplicação

--- src/components/SingInButton/index.tsx
+++ src/components/SingInButton/index.tsx
@@ -1,19 +1,28 @@
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
+import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SingInButton(): JSX.Element {
-  const isUserLoggedIn = true
+  const [session] = useSession()
 
-  return isUserLoggedIn ? (
-    <button type="button" className={styles.singInButton}>
+  return session ? (
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signOut()}
+    >
       <FaGithub color="#04d361" />
-      Alex Araujo
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button type="button" className={styles.singInButton}>
+    <button
+      type="button"
+      className={styles.singInButton}
+      onClick={() => signIn('github')}
+    >
       <FaGithub color="#eba417" />
       Sing in with Github
     </button>
